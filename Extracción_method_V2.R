40627df54d47d780f7548c1254cadcf77a56f71a
Bryan1qr <bquisper@unjbg.edu.pe>
2025-12-24 08:25:38 -0500

    version_dec_23th

--- Extracción_method_V2.R
+++ Extracción_method_V2.R
@@ -12,9 +12,28 @@ xy <- xy[,c(2,3)]
 names(xy) <- c('x','y')
 head(xy)
 
+#* Método 1: Utilizando el paquete geodata:
+climate <- worldclim_country(var = "bio",  res = 0.5, country = "Peru")
+
+# Extracción de los valores puntuales de las variables bioclimáticas
+# sobre los puntos de interés:
+Data_frame <- cbind(
+  raster::extract(
+    climate, xy, df = T),xy
+  )
+
+str(Data_frame)
+#* Guardamos
+write.csv (Data_frame, "world_clim_data_frame.csv")
+
+
+
+#* Método 2: Requiere descargar manualmente el zip
+#* de la página de WorldClim:
+
 #* Seleccionamos las imágenes y las ponemos en stack:
 data1 <- list.files(
-  path = "bio_clim",
+  path = "bio_clim", # nombre de la carpeta del descomprimido
   pattern = "*.asc",
   full.names = TRUE
 )
@@ -34,17 +53,3 @@ write.csv (Data_frame, "world_clim_data_frame.csv")
 
 #? Si los valoes salen NA es porque las imágenes no coinciden con la ubicación
 #? de las coordenadas
-
-
-#* Método 2: Probar cuando el servidor de geodata esté funcionando:
-
-climate <- worldclim_country(var = "bio",  res = 0.5, country = "Peru")
-#! The geodata server is temporary out of service for maintenance. It should be back on 20 December.
-Data_frame <- cbind(
-  raster::extract(
-    climate, xy, df = T),xy
-  )
-
-str(Data_frame)
-#* Guardamos
-write.csv (Data_frame, "world_clim_data_frame.csv")
